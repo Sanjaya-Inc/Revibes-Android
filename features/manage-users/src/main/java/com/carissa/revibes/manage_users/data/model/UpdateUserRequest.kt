77d3a976b6d53396fee889f31e79dfa3b65e7428
Fadillah Ramadhani <mozhaiqramadhani05@gmail.com>
2025-10-08 09:33:11 +0800

    feature(Users): Integrate Edit user with API

---
+++ features/manage-users/src/main/java/com/carissa/revibes/manage_users/data/model/UpdateUserRequest.kt
@@ -0,0 +1,22 @@
+package com.carissa.revibes.manage_users.data.model
+
+import androidx.annotation.Keep
+import kotlinx.serialization.Serializable
+
+@Keep
+@Serializable
+data class UpdateUserRequest(
+    val email: String? = null,
+    val displayName: String? = null,
+    val phoneNumber: String? = null,
+    val role: String? = null,
+    val points: Int? = null
+)
+
+@Keep
+@Serializable
+data class UpdateUserResponse(
+    val code: Int,
+    val message: String,
+    val status: String
+)
